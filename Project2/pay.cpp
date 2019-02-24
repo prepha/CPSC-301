1f1f5ea174eb81509d7cd9d3a16e79cd8ed76362
Preston <43216789+prepha@users.noreply.github.com>
2019-02-24 07:15:10 -0800

    Create pay.cpp

---
+++ Project2/pay.cpp
@@ -0,0 +1,67 @@
+#include "person.cpp"
+#include "person.h"
+#include <iostream>
+#include <fstream>
+
+void readData(Person ok[], int n)
+{
+  string firstName;
+  string lastName;
+  float payRate;
+  float hoursWorked;
+
+   std::fstream hello;
+  hello.open("input.txt");
+    for(int i=0; i<n; i++)
+    { 
+      
+      hello >>firstName >>lastName >> payRate >>hoursWorked;
+      ok[i].setFirstName(firstName);
+      ok[i].getFirstName();
+      
+      ok[i].setLastName(lastName);
+      ok[i].getLastName();
+
+      ok[i].fullName();
+
+
+      ok[i].setPayRate(payRate);
+      ok[i].getPayRate();
+
+      ok[i].setHoursWorked(hoursWorked);
+      ok[i].getHoursWorked();
+
+      cout<<ok[i].fullName()<< " ";
+
+      cout<<ok[i].totalPay()<<endl;
+      
+    }
+
+}
+
+void writeData(Person ok[])
+{
+ 
+}
+
+int main()
+{
+  
+
+     std::fstream hello;
+  hello.open("input.txt");
+
+  
+
+ int n =6;
+  Person ok[n];
+ 
+ cout<<"hello"<<endl;
+ 
+ 
+   readData(ok, n);
+   //writeData(ok);
+
+  hello.close();
+
+}
