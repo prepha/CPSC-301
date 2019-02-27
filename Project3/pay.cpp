a33aae2246e4cac0e8bdb995bde4fa6eea3b25d0
Preston <43216789+prepha@users.noreply.github.com>
2019-02-27 11:58:04 -0800

    Create pay.cpp

---
+++ Project3/pay.cpp
@@ -0,0 +1,38 @@
+#include "person.cpp"
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+void readData(vector<Person> &employees)
+{
+
+}
+
+void getCompanies(vector<Person> &employees, vector<Person> &companyNames)
+{
+
+}
+
+void printHighestPaid(vector<Person> &employees)
+{
+
+}
+
+void separateAndSave(vector<Person> &employees, vector<Person> &companyNames)
+{
+    
+}
+
+int main()
+{
+   vector<Person> employees;
+   vector<Person> companyNames;
+
+   void readData(vector<Person> employees);
+   void getCompanies(vector<Person> employees, vector<Person> companyNames);
+   void printHighestPaid(vector<Person> employees);
+   void separateAndSave(vector<Person> employees, vector<Person> companyNames);
+}
