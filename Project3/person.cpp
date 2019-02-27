f2a1c17d032f6d3ee27e9212efc83fde8c060dda
Preston <43216789+prepha@users.noreply.github.com>
2019-02-27 11:58:38 -0800

    Create person.cpp

---
+++ Project3/person.cpp
@@ -0,0 +1,79 @@
+#include "person.h"
+
+Person::Person()
+{
+
+}
+
+string Person::getFirstName() 
+{
+    return firstName;
+}
+void Person::setFirstName(string fName)
+{
+  firstName =fName;
+}
+
+string Person::getLastName()
+{
+    return lastName;
+}
+
+void Person::setLastName(string lName)
+{
+   lastName=lName;
+}
+
+
+int Person::getEmployeeId()
+{
+    return employeeID;
+}
+
+void Person::setEmployeeId(int id)
+{
+    employeeID=id;
+}
+
+string Person::getCompanyName()
+{
+    return companyName;
+}
+
+void Person::setCompanyName(string coName)
+{
+    companyName =coName;
+}
+
+float Person::getPayRate()
+{
+    return payRate;
+}
+
+void Person::setPayRate(float rate)
+{
+    payRate=rate;
+}
+
+float Person::getHoursWorked()
+{
+    return hoursWorked;
+}
+
+void Person::setHoursWorked(float hours)
+{
+    hoursWorked=hours;
+}
+
+float Person::totalPay()
+{
+    return hoursWorked*payRate;
+}
+
+string Person::fullName()
+{
+    return firstName + lastName;
+}
+
+
+
