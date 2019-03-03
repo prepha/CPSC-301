ae49b112619b308e47d9bc0686b5cda56b455d7d
Preston <43216789+prepha@users.noreply.github.com>
2019-03-03 15:48:31 -0800

    almost done

--- Project3/pay.cpp
+++ Project3/pay.cpp
@@ -83,28 +83,88 @@ for(int j=0; j<employees.size(); j++)
 
 void separateAndSave(vector<Person> &employees, vector<string> &companyNames)
 {
+   int n=0;
   
-   
-
+   float total=0.0;
+    std::fstream sup("Intel.txt");
+ 
    for(int p=0; p<employees.size(); p++)
-   {
+   { 
       if(employees.at(p).getCompanyName() == companyNames.front())
-      {
-       std::fstream sup("Intel.txt");
-         
-         cout<<employees.at(p).fullName()<< " "<<employees.at(p).getEmployeeId()<<" "<<companyNames.at(p)<< " "<<employees.at(p).totalPay() <<endl;
-         //cout<<employees.at(p).fullName()<<endl;
-         /*
-         sup<<employees[p].fullName()<<endl;
-         sup<<employees[p].getEmployeeId()<<endl;
-         sup<<employees[p].getCompanyName()<<endl;
-         sup<<employees[p].totalPay()<<endl;
-         */
+      {  
+      cout<<employees.at(p).fullName()<< " "<<employees.at(p).getEmployeeId()<<" "<<companyNames.at(p)<< " "<<employees.at(p).totalPay() <<endl;
+      total+=employees.at(p).totalPay();
+      sup<<employees.at(p).fullName()<< " "<<employees.at(p).getEmployeeId()<<" "<<companyNames.at(p)<< " "<<employees.at(p).totalPay() <<endl;
       }  
-     
+   }
+       cout<<total<<endl;
+       sup<<"Total:"<<" "<<total<<endl;
+
+
+int s=1;
+   std::fstream hello("Boeing.txt");
+   for(int k=0; k<employees.size(); k++)
+   {
+      if(employees.at(k).getCompanyName() == companyNames.at(s))
+      { 
+      cout<<employees.at(k).fullName()<< " "<<employees.at(k).getEmployeeId()<<" "<<companyNames.at(s)<< " "<<employees.at(k).totalPay() <<endl; 
+      total+=employees.at(k).totalPay();
+      hello<<employees.at(k).fullName()<< " "<<employees.at(k).getEmployeeId()<<" "<<companyNames.at(s)<< " "<<employees.at(k).totalPay() <<endl; 
       }
    }
-   
+      cout<<total<<endl;
+       hello<<"Total:"<<" "<<total<<endl;
+
+
+   int d =2;
+   std::fstream bonjour("Douglas.txt");
+   for(int e=0; e<employees.size(); e++)
+   {
+      if(employees.at(e).getCompanyName() == companyNames.at(d))
+      {
+      cout<<employees.at(e).fullName()<< " "<<employees.at(e).getEmployeeId()<<" "<<companyNames.at(e)<< " "<<employees.at(e).totalPay() <<endl; 
+      total+=employees.at(e).totalPay();
+      hello<<employees.at(e).fullName()<< " "<<employees.at(e).getEmployeeId()<<" "<<companyNames.at(e)<< " "<<employees.at(e).totalPay() <<endl; 
+
+      }
+   }
+      cout<<total<<endl;
+       bonjour<<"Total:"<<" "<<total<<endl;
+
+
+
+int r=3;
+std::fstream hola("Raytheon.txt");
+for(int b=0; b<employees.size(); b++)
+{
+      if(employees.at(b).getCompanyName() == companyNames.at(r))
+      {
+          cout<<employees.at(b).fullName()<< " "<<employees.at(b).getEmployeeId()<<" "<<companyNames.at(r)<< " "<<employees.at(b).totalPay() <<endl; 
+          total+=employees.at(b).totalPay();
+          hola<<employees.at(b).fullName()<< " "<<employees.at(b).getEmployeeId()<<" "<<companyNames.at(b)<< " "<<employees.at(b).totalPay() <<endl; 
+      }
+}
+         cout<<total<<endl;
+       hola<<"Total:"<<" "<<total<<endl;
+
+
+
+ int y=8;
+ std::fstream chao("HealthTech.txt");
+
+ for(int u=0; u<employees.size(); u++)
+{
+      if(employees.at(u).getCompanyName() == companyNames.at(y))
+      {
+          cout<<employees.at(u).fullName()<< " "<<employees.at(u).getEmployeeId()<<" "<<companyNames.at(u)<< " "<<employees.at(u).totalPay() <<endl; 
+          total+=employees.at(u).totalPay();
+          chao<<employees.at(u).fullName()<< " "<<employees.at(u).getEmployeeId()<<" "<<companyNames.at(u)<< " "<<employees.at(u).totalPay() <<endl; 
+      }
+}
+         cout<<"Total:"<< " "<<total<<endl;
+       chao<<"Total:"<<" "<<total<<endl;
+}
+
     
 
 
@@ -120,16 +180,7 @@ int main()
    getCompanies(employees,companyNames);
    // printHighestPaid(employees);
     separateAndSave( employees, companyNames);
-/*
-if(employees.empty())
-{
-   employees.push_back(ok);
-   employees.push_back(ok);
-   employees.push_back(ok);
-   employees.push_back(ok);
-   employees.push_back(ok);
-}
-*/
+
 hello.close();
    
 }
