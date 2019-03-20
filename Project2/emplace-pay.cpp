40c6ec1d058124172fb056874c3b316a1b914b4e
Preston <43216789+prepha@users.noreply.github.com>
2019-03-19 21:13:21 -0700

    numbers output but names dont

--- Project2/emplace-pay.cpp
+++ Project2/emplace-pay.cpp
@@ -2,91 +2,52 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include "person.h"
 
 
-void readData(vector<Person> ok,int n)
+
+void readData(vector<Person> &ok)
 {
   string firstName;
   string lastName;
-  float payRate;
-  float hoursWorked;
-  
-  int count=0;
- 
+  float payRate =0.0;
+  float hoursWorked=0.0;
    std::fstream hello;
   hello.open("input.txt");
+  while(hello >>firstName >>lastName >> payRate >>hoursWorked)
+  {
+    Person().setFirstName(firstName);  
+    Person().setLastName(lastName);
+    Person().setPayRate(payRate);
+    Person().setHoursWorked(hoursWorked);
+    ok.emplace_back(Person(firstName,lastName,payRate,hoursWorked));
+  }
 
  
-
- // ok.emplace_back(Person(firstName,lastName,payRate,hoursWorked));
- while(hello >>firstName >>lastName >> payRate >>hoursWorked)
- {
-   
-
-  Person().setFirstName(firstName);  
-  Person().setLastName(lastName);
-  Person().setPayRate(payRate);
-  Person().setHoursWorked(hoursWorked);
-  
-
-  ok.emplace_back(Person(firstName,lastName,payRate,hoursWorked));
- // ok.emplace_back(Person().getFirstName(),Person().getLastName(),Person().getPayRate(),Person().getHoursWorked());
-  
-  count++;
-}
-
- cout<<ok.at(0).getFirstName(); 
-cout<<count<<endl;
-
-  cout<<ok.size()<<endl;  
-
-
-    for(int i=0; i<n; i++)
-    { 
-
-      cout<<ok.at(i).getFirstName();
-      //hello >>firstName >>lastName >> payRate >>hoursWorked;
-  /*
-      ok.at(i).setFirstName(firstName);
-      
-      ok.at(i).setLastName(lastName);
-     
-       ok.at(i).setPayRate(payRate);
-    
-      ok.at(i).setHoursWorked(hoursWorked);
-*/
-   // ok.emplace_back(ok[i].getFirstName(),ok[i].getLastName(),ok[i].getPayRate(), ok[i].getHoursWorked());
-     
-     // ok.emplace_back(Person().getFirstName(),Person().getLastName(),Person().getPayRate(),Person().getHoursWorked());
-      // cout<<ok.at(i).getFirstName();
-      cout<<"hello";
-    }
-  
+ hello.close();
 
 }
 
-void writeData(vector<Person> ok)
+void writeData(vector<Person> &ok)
 {
   std::fstream sup ("output.txt");
   
  
  for(int i=0; i <ok.size(); i++)
  { 
+      sup<<ok.at(i).getFirstName();
+      sup<<ok.at(i).getLastName();
+
      sup<<ok.at(i).fullName()<< " ";
 
     sup<<ok.at(i).totalPay()<<endl;
+
+    cout<<ok.at(i).getFirstName()<<endl;
 
     cout<<ok.at(i).fullName()<< " ";
 
       cout<<ok.at(i).totalPay()<<endl;
 
 
-      if(ok[i].fullName() == ok[i+1].fullName())
-      {
-        break;
-      }
-   
  }
 }
 
@@ -97,13 +58,13 @@ int main()
   hello.open("input.txt");
 
 //Person ok[n];
- int n =20;
+ //int n =20;
  vector<Person> ok;
   
  
-   readData(ok,n);
+   readData(ok);
    writeData(ok);
 
-  hello.close();
+ 
 
 }
