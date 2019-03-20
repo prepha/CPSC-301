305d9a6170f2cf9ac1a0279ade738d03add889fa
Preston <43216789+prepha@users.noreply.github.com>
2019-03-20 07:58:26 -0700

    finished

--- Project2/emplace-pay.cpp
+++ Project2/emplace-pay.cpp
@@ -1,70 +1,66 @@
-#include "person.cpp"
-#include <iostream>
-#include <fstream>
-#include <vector>
-
-
-
-void readData(vector<Person> &ok)
+#include "person.h"
+Person::Person()
 {
-  string firstName;
-  string lastName;
-  float payRate =0.0;
-  float hoursWorked=0.0;
-   std::fstream hello;
-  hello.open("input.txt");
-  while(hello >>firstName >>lastName >> payRate >>hoursWorked)
-  {
-    Person().setFirstName(firstName);  
-    Person().setLastName(lastName);
-    Person().setPayRate(payRate);
-    Person().setHoursWorked(hoursWorked);
-    ok.emplace_back(Person(firstName,lastName,payRate,hoursWorked));
-  }
-
- 
- hello.close();
 
 }
 
-void writeData(vector<Person> &ok)
+Person::Person(string fName, string lName, float rate, float hours)
 {
-  std::fstream sup ("output.txt");
-  
- 
- for(int i=0; i <ok.size(); i++)
- { 
-      sup<<ok.at(i).getFirstName();
-      sup<<ok.at(i).getLastName();
-
-     sup<<ok.at(i).fullName()<< " ";
-
-    sup<<ok.at(i).totalPay()<<endl;
-
-    cout<<ok.at(i).getFirstName()<<endl;
-
-    cout<<ok.at(i).fullName()<< " ";
-
-      cout<<ok.at(i).totalPay()<<endl;
-
-
- }
-}
-
-int main()
-{
-
-     std::fstream hello;
-  hello.open("input.txt");
-
-//Person ok[n];
- //int n =20;
- vector<Person> ok;
-  
- 
-   readData(ok);
-   writeData(ok);
-
- 
+   firstName=fName;
+   lastName=lName;
+   payRate=rate;
+   hoursWorked=hours;
 
 }
+string Person::getFirstName()
+{
+    return firstName;
+}
+
+void Person::setFirstName(string fName)
+{
+   firstName = fName;
+}
+
+string Person::getLastName()
+{
+    return lastName;
+}
+
+void Person::setLastName(string lName)
+{
+   lastName =lName;
+}
+
+float Person::getPayRate()
+{
+    return payRate;
+}
+
+void Person::setPayRate(float rate)
+{
+    payRate = rate;
+}
+
+float Person::getHoursWorked()
+{
+    return hoursWorked;
+}
+
+void Person::setHoursWorked(float hours)
+{
+    hoursWorked = hours;
+}
+
+float Person::totalPay()
+{
+    return hoursWorked * payRate;
+}
+
+string Person::fullName()
+{
+    return firstName + " " +lastName;
+}
+
+
+
