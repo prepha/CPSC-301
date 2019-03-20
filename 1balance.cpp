32b9bd54093c3590f22ac7f34b98f4141b36b700
Preston <43216789+prepha@users.noreply.github.com>
2019-03-19 19:26:21 -0700

    Update 1balance.cpp

--- 1balance.cpp
+++ 1balance.cpp
@@ -31,13 +31,15 @@ void Display(Person P[],int n)
     hello>>name2;
    
     hello>>num;
-    
+
     sname = name1 + " " + name2;
     
     strcpy(P[i].Name,sname.c_str());
     P[i].balance=num;
     cout<<P[i].Name<< " ";
     cout<<P[i].balance<<endl;
+
+   
   }
   hello.close();  
 }
@@ -64,14 +66,13 @@ void FindRichest(Person P[], int n)
       cout<<"Person with highest value is "<<P[i].Name<<endl;
     }
   }
-
   hello.close();
 
 }
 
 
 //void Deposit(char* CustName, Person P[],int n)
-void Deposit(Person arr[], int n,char *CustName, float amount)
+void Deposit(Person P[], int n,char *CustName, float amount)
 {
  std::fstream hello; 
 
@@ -81,13 +82,12 @@ void Deposit(Person arr[], int n,char *CustName, float amount)
  cout<<"How much would you like to deposit"<<endl;
  cin>>amount;
  
- 
   for (int i=0; i<n; i++)
   {
-    if(strcmp(CustName,arr[i].Name) == 0)
+    if(strcmp(CustName,P[i].Name) == 0)
     {
-        amount+=arr[i].balance; 
-        arr[i].balance=amount;
+        amount+=P[i].balance; 
+        P[i].balance=amount;
     }
    
   }
@@ -132,9 +132,8 @@ void NewCopy(std::fstream& hello,Person P[],int n)
     personptr[i].balance=num;
     cout<<personptr[i].Name<< " ";
     cout<<personptr[i].balance<<endl;
-
-  return personptr;
   }
+  return personptr;
   hello.close();  
 }
 
@@ -161,19 +160,22 @@ int n =entry;
 
 Person *personptr;
   personptr = new Person[n];
-  Person *arr;
-  arr = new Person[n];
+ 
 
-  arr = personptr;
+  //personptr=P[n];
+
+  
 
 
   char CustName[20];
   Display(P,n);
   FindRichest(P,n);
  // Deposit(CustName,P,n);
-  Deposit(arr, n,CustName,amount);
+  Deposit(P,n,CustName,amount);
   NewCopy(hello,P,n);
   readData(n);
+
+
 
   hello.close();
 
