52990c7964e737a6256a6898c6b7b1bb48d16434
Preston <43216789+prepha@users.noreply.github.com>
2019-03-19 19:26:46 -0700

    Update balance.cpp

--- balance.cpp
+++ balance.cpp
@@ -31,13 +31,15 @@ void Display(Person P[],int n)
     hello>>name2;
    
     hello>>num;
-    
+
     sname = name1 + " " + name2;
     
     strcpy(P[i].Name,sname.c_str());
     P[i].balance=num;
     cout<<P[i].Name<< " ";
     cout<<P[i].balance<<endl;
+
+   
   }
   hello.close();  
 }
@@ -64,41 +66,37 @@ void FindRichest(Person P[], int n)
       cout<<"Person with highest value is "<<P[i].Name<<endl;
     }
   }
-
   hello.close();
 
 }
 
 
-void Deposit(char* CustName, Person P[],int n)
+//void Deposit(char* CustName, Person P[],int n)
+void Deposit(Person P[], int n,char *CustName, float amount)
 {
  std::fstream hello; 
- float deposit = 0.0;
- 
 
  cout<<"Enter Full name to deposit:";
  cin.getline(CustName,20);
 
  cout<<"How much would you like to deposit"<<endl;
- cin>>deposit;
- 
+ cin>>amount;
  
   for (int i=0; i<n; i++)
   {
     if(strcmp(CustName,P[i].Name) == 0)
     {
-        deposit+=P[i].balance; 
-        P[i].balance=deposit;
+        amount+=P[i].balance; 
+        P[i].balance=amount;
     }
    
   }
-cout<<"Their new balance is " <<deposit<<endl;
+cout<<"Their new balance is " <<amount<<endl;
 
 }
 
 void NewCopy(std::fstream& hello,Person P[],int n)
 {
-
   for(int i=0; i<n; i++)
   {
     hello<<P[i].Name;
@@ -109,8 +107,35 @@ void NewCopy(std::fstream& hello,Person P[],int n)
   }
 } 
  
+ Person *readData(int &n)
+{
+  Person *personptr;
+  personptr = new Person[n];
+  std::fstream hello;
+  hello.open("data.txt");
+ 
+ string name1;
+ string name2;
+ string sname;
+ float num;
 
-
+ Person ok;
+  for(int i =0; i<n; i++)
+  {
+    hello>>name1;
+    hello>>name2;
+    hello>>num;
+    
+    sname = name1 + " " + name2;
+    
+    strcpy(personptr[i].Name,sname.c_str());
+    personptr[i].balance=num;
+    cout<<personptr[i].Name<< " ";
+    cout<<personptr[i].balance<<endl;
+  }
+  return personptr;
+  hello.close();  
+}
 
 int main()
 {
@@ -124,18 +149,32 @@ int main()
       hello>>ok.Name;
       hello.getline(ok.Name, 20);
        entry++;
-      }
+  }
 int n =entry;
 
 
-
  Person P[n];
+// Person arr[n];
+ float amount = 0.0;
+ 
+
+Person *personptr;
+  personptr = new Person[n];
+ 
+
+  //personptr=P[n];
+
+  
+
 
   char CustName[20];
   Display(P,n);
   FindRichest(P,n);
-  Deposit(CustName,P,n);
+ // Deposit(CustName,P,n);
+  Deposit(P,n,CustName,amount);
   NewCopy(hello,P,n);
+  readData(n);
+
 
 
   hello.close();
