4b4487bbf5e8c121c0f4e28edc94d12e94823605
Preston <43216789+prepha@users.noreply.github.com>
2019-05-02 21:32:14 -0700

    Create linkedlist.h

---
+++ project7/linkedlist.h
@@ -0,0 +1,31 @@
+//begin linkedlist.h
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <string>
+
+using namespace std;
+
+struct node {
+    int data;
+    node * next;
+};
+
+class linkedlist {
+private:
+    node * head;
+   
+    int count(); // RECURSIVE
+    int sum(); // RECURSIVE
+    int counter=0;
+    int total =0;
+
+public:
+    linkedlist();
+    ~linkedlist(); // RECURSIVE
+    void addToFront(int n);
+    double average(); // uses sum and count functions
+    void writeInorder(string & file);
+    void writeReversed(string & file);
+};
+#endif // end linkedlist.h
